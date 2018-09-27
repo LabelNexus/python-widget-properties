a15c2b4bba52b52905e5f44a82ad180445984fc7
Bret Scarlavai <bret@northquad.com>
2018-09-27 14:50:29 +0000

    properties update the component metadata to indicate they are using the component set

--- properties/component.py
+++ properties/component.py
@@ -49,8 +49,12 @@ class ComponentPropertyType(BasePropertyType):
       display_name_template = None
 
       if component.component_type in g.all_components:
-        result['componentTemplate'] = g.all_components.get(component.component_type, {}).get('template', '')
-        display_name_template = g.all_components.get(component.component_type, {}).get('displayNameTemplate')
+        component_meta = g.all_components.get(component.component_type, {})
+        # Flag the component as being used so widget proxy can update component set namespace version table
+        component_meta.update({'isUsed': True})
+
+        result['componentTemplate'] = component_meta.get('template', '')
+        display_name_template = component_meta.get('displayNameTemplate')
 
       if display_name_template:
         rtemplate = Environment(loader=BaseLoader).from_string(display_name_template)
