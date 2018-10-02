53f2d2ccd95d129036db5ce33d5e284f80234288
Gunnar Norred <gnorred88@gmail.com>
2018-10-02 18:44:48 +0000

    Roll back the last commit

--- properties/dropdown.py
+++ properties/dropdown.py
@@ -20,13 +20,13 @@ class DropdownPropertyType(BasePropertyType):
   def read(self, data):
     val = data.get(self.property.name)
 
-    check_hash = { str(x).lower(): x.lower() for x in self.property.options }
+    check_hash = { str(x): x for x in self.property.options }
     check_hash['None'] = None
 
     if val is None:
       val = self.property.default
 
-    if str(val).lower() not in check_hash:
+    if str(val) not in check_hash:
       raise ValidationException('Invalid Value: ' + str(val) + ' - must be in set ' + ','.join(list(self.property.options.keys())))
 
     return val
