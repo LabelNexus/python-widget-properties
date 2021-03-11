ef7389a3208cf5dea51d562f2a69791577d311a2
Eric Seidel <e.seidel@lumavate.com>
2021-03-11 10:49:14 -0500

    fixed property reference

--- properties/text.py
+++ properties/text.py
@@ -28,8 +28,8 @@ class TextPropertyType(BasePropertyType):
     val = super().read(data)
 
     if self.required and (val is None or val == ''):
-      raise ValidationException(f'Field {self.name} is required')
+      raise ValidationException(f'Field {self.property.name} is required')
 
     if self.maxlength > 0 and val is not None and len(val) > self.maxlength:
-      raise ValidationException(f'Field {self.name} cannot be more than {self.maxlength} characters long.')
+      raise ValidationException(f'Field {self.property.name} cannot be more than {self.maxlength} characters long.')
     return val
