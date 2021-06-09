617f3f9342e232c1019a8d190e60bab617a1d35d
Eric Seidel <e.seidel@lumavate.com>
2021-06-08 21:55:32 -0400

    server side keyinput validation

--- properties/text.py
+++ properties/text.py
@@ -1,5 +1,7 @@
+import re
 from .base_property_type import BasePropertyType
 from lumavate_exceptions import ValidationException
+
 
 class TextPropertyType(BasePropertyType):
   @property
@@ -21,6 +23,13 @@ class TextPropertyType(BasePropertyType):
     return self.property.options.get('maxlength', 0)
 
   @property
+  def allowed_keys(self):
+    if self.property.options is None:
+      return 'all'
+
+    return self.property.options.get('allowedKeys', 'all')
+
+  @property
   def minlength(self):
     return self.property.options.get('minlength', 0)
 
@@ -32,4 +41,13 @@ class TextPropertyType(BasePropertyType):
 
     if self.maxlength > 0 and val is not None and len(val) > self.maxlength:
       raise ValidationException(f'Field {self.property.name} cannot be more than {self.maxlength} characters long.', api_field=self.property.name)
+
+    if self.allowed_keys != 'all':
+      if self.allowed_keys == 'alpha' and not val.isalpha():
+        raise ValidationException(f'Field {self.property.name} must contain only letters.', api_field=self.property.name)
+      if self.allowed_keys == 'alpha-numeric' and not val.isalnum():
+        raise ValidationException(f'Field {self.property.name} must contain only letters and numbers.', api_field=self.property.name)
+      if self.allowed_keys == 'alpha-numeric-space' and not re.search('[a-zA-Z0-9 ]', val):
+        raise ValidationException(f'Field {self.property.name} must contain only letters, numbers, and spaces.', api_field=self.property.name)
+
     return val
