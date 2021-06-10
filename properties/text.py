2b8f18e7722397712b6d04cfcb55e50d733a9e9b
ericseidel <34065018+ericseidel@users.noreply.github.com>
2021-06-10 09:50:10 -0400

    PLAT3-947 text input validations (#25)
    * server side keyinput validation
    
    * New validations for text property options alpha, alpha-numeric, alpha-numeric-space, and alpha-numeric-extra
    
    * Updated text validations
    
    * Removed unused re library

--- properties/text.py
+++ properties/text.py
@@ -1,6 +1,7 @@
 from .base_property_type import BasePropertyType
 from lumavate_exceptions import ValidationException
 
+
 class TextPropertyType(BasePropertyType):
   @property
   def type_name(self):
@@ -21,6 +22,13 @@ class TextPropertyType(BasePropertyType):
     return self.property.options.get('maxlength', 0)
 
   @property
+  def allowed_keys(self):
+    if self.property.options is None:
+      return 'all'
+
+    return self.property.options.get('allowedKeys', 'all')
+
+  @property
   def minlength(self):
     return self.property.options.get('minlength', 0)
 
@@ -28,8 +36,28 @@ class TextPropertyType(BasePropertyType):
     val = super().read(data)
 
     if self.required and (val is None or val == ''):
-      raise ValidationException(f'Field {self.property.name} is required', api_field=self.property.name)
+      raise ValidationException(f'Field is required', api_field=self.property.name)
 
     if self.maxlength > 0 and val is not None and len(val) > self.maxlength:
-      raise ValidationException(f'Field {self.property.name} cannot be more than {self.maxlength} characters long.', api_field=self.property.name)
+      raise ValidationException(f'Field cannot be more than {self.maxlength} characters long.', api_field=self.property.name)
+
+    if self.allowed_keys != 'all':
+      if self.allowed_keys == 'alpha' and not val.isalpha():
+        raise ValidationException(f'Field may contain only letters.', api_field=self.property.name)
+      if self.allowed_keys == 'alpha-numeric' and not val.isalnum():
+        raise ValidationException(f'Field may contain only letters and numbers.', api_field=self.property.name)
+      if self.allowed_keys == 'alpha-numeric-space' and not self.isAlphaNumericSpace(val):
+        raise ValidationException(f'Field may contain only letters, numbers, and spaces.', api_field=self.property.name)
+      if self.allowed_keys == 'alpha-numeric-extra' and not self.isAlphaNumericExtra(val):
+        raise ValidationException(f'Field may contain only letters, numbers, space, dash, and underscore.', api_field=self.property.name)
+
     return val
+
+  def isAlphaNumericSpace(self, val):
+    return all(x.isalnum() or x.isspace() for x in val)
+
+  def isAlphaNumericExtra(self, val):
+    return all(x.isalnum() or x.isspace() or x=='-' or x=='_' for x in val)
+
+
+
