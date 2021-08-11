0a46e0a58fcd15425d6681cf1cbf1da4e739b06c
Fareed <f.shaik@lumavate.com>
2021-08-11 13:08:59 -0700

    chnges

--- properties/html_editor.py
+++ properties/html_editor.py
@@ -7,13 +7,4 @@ class HtmlEditorPropertyType(TextPropertyType):
   def type_name(self):
     return 'html-editor'
 
-  def read(self, data):
-    val = super().read(data)
-
-    if val is None:
-      val = {'en-us': ''}
-
-    elif not isinstance(val, dict):
-      val = {'en-us': str(val)}
-
-    return val
+  
