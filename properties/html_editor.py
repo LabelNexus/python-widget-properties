9020a422c66876316880f8ab297d6d51d9d5928e
Fareed <f.shaik@lumavate.com>
2021-08-11 14:06:36 -0700

    changes

--- properties/html_editor.py
+++ properties/html_editor.py
@@ -6,7 +6,8 @@ class HtmlEditorPropertyType(TextPropertyType):
   @property
   def type_name(self):
     return 'html-editor'
-def read(self, data):
+
+  def read(self, data):
     val = super().read(data)
 
     if val is None:
