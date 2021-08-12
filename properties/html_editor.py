8e4e260678f435a17aaa0d1d84695cdcf80e6517
Fareed <f.shaik@lumavate.com>
2021-08-12 08:26:09 -0700

    replaced text property with translated text property

--- properties/html_editor.py
+++ properties/html_editor.py
@@ -1,19 +1,9 @@
-from .text import TextPropertyType
+from .translated_text import TranslatedTextProperty
+
 from lumavate_exceptions import ValidationException
 import re
 
-class HtmlEditorPropertyType(TextPropertyType):
+class HtmlEditorPropertyType(TranslatedTextProperty):
   @property
   def type_name(self):
     return 'html-editor'
-
-  def read(self, data):
-    val = super().read(data)
-
-    if val is None:
-      val = {'en-us': ''}
-
-    elif not isinstance(val, dict):
-      val = {'en-us': str(val)}
-
-    return val
