026f3ad095a9e2a222f40ed4ecd64918583e73dd
ericseidel <34065018+ericseidel@users.noreply.github.com>
2022-04-08 09:27:03 -0400

    PLAT3-1830 new font style property (#38)
    * PLAT3-1830 new font style property
    
    * PLAT3-1830 fixed typo
    
    * PLAT3-1830 updated color and includeUnderline defaults if one isn't included.
    
    * PLAT3-1830 underline default

---
+++ properties/font_style.py
@@ -0,0 +1,60 @@
+from .base_property_type import BasePropertyType
+from lumavate_exceptions import ValidationException
+
+class FontStylePropertyType(BasePropertyType):
+  @property
+  def type_name(self):
+    return 'font-style'
+
+  @property
+  def default_font_family(self):
+    if self.property.default is None:
+      return 'primary'
+
+    return self.property.default.get('fontFamily', 'primary')
+
+  @property
+  def default_font_size(self):
+    if self.property.default is None:
+      return 16
+
+    return self.property.default.get('fontSize', 16)
+
+  @property
+  def default_font_color(self):
+    if self.property.default is None:
+      return 'var(--primary-color)'
+
+    return self.property.default.get('fontColor', 'var(--primary-color)')
+
+  @property
+  def default_underline(self):
+    if self.property.default is None:
+      return False
+
+    return self.property.default.get('underline', False)
+  @property
+  def include_underline(self):
+    if self.property.options is None:
+      return False
+
+    return self.property.options.get('includeUnderline', False)
+
+
+  def read(self, data):
+    val = super().read(data)
+
+    if not val.get('fontSize'):
+      val['fontSize'] = self.default_font_size
+
+    if not val.get('fontColor'):
+      val['fontColor'] = self.default_font_color
+
+    if not val.get('fontFamily'):
+      val['fontFamily'] = self.default_font_family
+
+    if not val.get('underline'):
+      val['underline'] = self.default_font_family
+
+    return val
+
