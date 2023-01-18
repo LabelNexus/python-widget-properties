00710c78adcb9ddd64d9cd330bbcd7c815d76f5d
Eric Seidel <e.seidel@lumavate.com>
2023-01-18 11:05:39 -0500

    PLAT3-2375 decimal places validation fix

--- properties/numeric.py
+++ properties/numeric.py
@@ -34,10 +34,11 @@ class NumericPropertyType(BasePropertyType):
       if self.decimal_places == 0:
         val = int(val)
       else:
-        val = float(format(val, f'.{self.decimal_places}f'))
-
+        # format then save as float truncated to max of number of decimals
+        # since we dont have a separate output read function for formatting data
+        val = float(format(float(val), f'.{self.decimal_places}f'))
     except Exception as e:
-      raise ValidationException('Invalid Number: ' + str(val))
+      raise ValidationException('Invalid Number: ' + str(val), api_field=self.property.name)
 
     if (self.too_low(val)) or (self.too_high(val)):
       raise ValidationException('Invalid Number: ' + str(val) + \
